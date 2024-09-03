40283ff71d7b0120997ddfb371e91506e47a5172
Thomas <89225766+Schattenoff@users.noreply.github.com>
2024-09-03 11:17:11 +0300

    update links

--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@ const table = urlParams.get('t');
 const aziatLink = document.getElementById('aziat');
 const farshLink = document.getElementById('farsh');
 
-aziatLink.href = `https://qr.foodpicasso.com/aziatkvartal?&t=${table}&r=qr`;
-farshLink.href = `https://qr.foodpicasso.com/farshs?&t=${table}`;
+aziatLink.href = `https://qr.foodpicasso.com/aziatkvartal?address=ef622&t=${table}`;
+farshLink.href = `https://qr.foodpicasso.com/farshs?address=f8829&t=${table}`;
